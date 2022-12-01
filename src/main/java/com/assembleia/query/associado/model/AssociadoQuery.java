626f25f06dbeff1ec43caa071510d594f3ff27b0
Douglas Cidral <douglas.cidral@cadmus.com.br>
2022-12-01 10:01:25 -0300

    removing deleted associados from get

--- src/main/java/com/assembleia/query/associado/model/AssociadoQuery.java
+++ src/main/java/com/assembleia/query/associado/model/AssociadoQuery.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -23,6 +24,7 @@ import java.util.UUID;
 @Immutable
 @Entity
 @Table(name = "associado")
+@Where(clause = "deleted=false")
 public class AssociadoQuery extends AbstractAnemicEntity<UUID> {
 
     @Id
